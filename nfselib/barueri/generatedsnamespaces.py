f12181e78dd706b6fea13525512fda4d7e3bc2c0
André Marcos Ferreira <andremarcosfe@gmail.com>
2023-04-13 04:04:41 -0300

    [WIP] GenerateDS

---
+++ nfselib/barueri/generatedsnamespaces.py
@@ -0,0 +1,22 @@
+#TODO
+
+GenerateDSNamespaceDefs = {
+    "ConsultaNFeRecebidaCompetencia": 'xmlns="" xmlns:tipos=""',
+    "ConsultaNFeRecebidaNumero": 'xmlns="d" xmlns:tipos=""',
+    "ConsultaNFeRecebidaPeriodo": 'xmlns=""',
+    "NFeLoteBaixarArquivo": 'xmlns="" xmlns:tipos=""',
+    "NFeLoteListarArquivos": 'xmlns="" xmlns:tipos=""',
+    "NFeLoteStatusArquivo": 'xmlns:p="" xmlns:p1=""',
+    "NFeLoteEnviarArquivo" : 'xmlns:="https://www.barueri.sp.gov.br/nfeservice/wsrps.asmx?op=NFeLoteListarArquivos" xmlns:""'
+}
+
+# GenerateDSNamespaceTypePrefixes = {
+#     "tcLoteRps": 'tipos',
+#     "tcIdentificacaoPrestador": 'tipos',
+#     "tcIdentificacaoRps": 'tipos',
+#     "tcIdentificacaoNfse": 'p1',
+#     "tcInfPedidoCancelamento": 'p1',
+#     "tcPedidoCancelamento": 'p1',
+#     "CancelarNfseEnvio": 'p',
+#     "cabecalho": 'ns2'
+# }
